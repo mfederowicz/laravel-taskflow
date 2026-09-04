c6ba94103a3a228b42fa59056cd6587a383ca676
Marcin Federowicz <marcin.federowicz@gmail.com>
2026-09-05 00:28:28 +0200

    backend: project: models and migrations

--- app/backend/app/Models/User.php
+++ app/backend/app/Models/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Task::class);
     }
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
