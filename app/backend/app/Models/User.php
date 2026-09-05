3011dc8513446653cf4c50970cfaefb202ff4f49
Marcin Federowicz <marcin.federowicz@gmail.com>
2026-09-05 15:40:02 +0200

    backend: comments : model and migrations

--- app/backend/app/Models/User.php
+++ app/backend/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Project::class);
     }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
