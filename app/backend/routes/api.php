bc405a887cd20a0e4ec67b6855bac3ebd13427bd
Marcin Federowicz <marcin.federowicz@gmail.com>
2026-09-06 13:46:11 +0200

    auth: all protected routes using 'auth.multi' middleware

--- app/backend/routes/api.php
+++ app/backend/routes/api.php
@@ -16,46 +16,29 @@ Route::get('/health', function () {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/login/jwt', [AuthController::class, 'loginJwt']);
-
-Route::middleware('auth:jwt')->get('/jwt-test', function (Request $request) {
-    return response()->json([
-        'success' => true,
-        'user' => $request->user(),
-    ]);
-});
-
-Route::middleware('auth:passport')->get('/passport-test', function (Request $request) {
-    return response()->json([
-        'success' => true,
-        'user' => $request->user(),
-    ]);
-});
 
 Route::middleware('auth.multi')->group(function () {
-    Route::get('/tasks', [TaskController::class, 'index']);
-});
 
-
-Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
 
-    Route::post('/logout', [AuthController::class, 'logout']);
-
-    //Route::get('/tasks', [TaskController::class, 'index']);
+    // Tasks
+    Route::get('/tasks', [TaskController::class, 'index']);
     Route::post('/tasks', [TaskController::class, 'store']);
     Route::get('/tasks/{task}', [TaskController::class, 'show']);
     Route::put('/tasks/{task}', [TaskController::class, 'update']);
     Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
 
+    // Projects
     Route::get('/projects', [ProjectController::class, 'index']);
     Route::post('/projects', [ProjectController::class, 'store']);
     Route::get('/projects/{project}', [ProjectController::class, 'show']);
     Route::put('/projects/{project}', [ProjectController::class, 'update']);
     Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
 
+    // Comments
     Route::get('/tasks/{task}/comments', [CommentController::class, 'index']);
     Route::post('/tasks/{task}/comments', [CommentController::class, 'store']);
 });
