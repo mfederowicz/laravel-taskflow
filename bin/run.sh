7d7d21c5ed5e099f49b6588779c8a1755db960f1
Marcin Federowicz <marcin.federowicz@gmail.com>
2026-09-07 23:07:06 +0200

    docker: fix default user ownership

--- bin/run.sh
+++ bin/run.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+export USER_UID=$(id -u)
+export USER_GID=$(id -g www-data)
+
 set -e
+
+
 
 COMPOSE="docker compose --env-file .env -f docker/docker-compose.yml"
 
 case "${1:-start}" in
     start)
+        #chmod -R ug+rwX app/backend/storage app/backend/bootstrap/cache
         $COMPOSE up -d
         ;;
 
